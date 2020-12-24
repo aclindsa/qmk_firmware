7218abbdd09aada7d04685a36eed6237e1a44cd1
camrbuss <camrbuss@vt.edu>
2020-12-24 10:13:32 -0700

    Grabert gol implemented on init

--- keyboards/kobuss/grabert/grabert.c
+++ keyboards/kobuss/grabert/grabert.c
@@ -16,14 +16,18 @@
 
 #include "grabert.h"
 
-static uint32_t oled_timer    = 0;
-static bool     write_outline = true;
+static uint32_t oled_timer                       = 0;
+static uint8_t  gol_state[GOL_WIDTH][GOL_HEIGHT] = {0};
+static uint32_t gol_update_count                 = 0;
+
+void keyboard_pre_init_user(void) { gol_randomize(); }
 
 void suspend_power_down_user(void) { oled_off(); }
 
 void suspend_wakeup_init_user(void) {
-    oled_timer    = timer_read32();
-    write_outline = true;
+    oled_timer       = timer_read32();
+    gol_update_count = 0;
+    gol_randomize();
 }
 
 void oled_task_user(void) {
@@ -31,77 +35,134 @@ void oled_task_user(void) {
         /* Display a logo for a short period of time after  */
         /* turning the keyboard on */
         oled_write_raw(kobuss_logo, OLED_MATRIX_SIZE);
+    } else if (gol_update_count < GOL_UPDATE_MAX_COUNT) {
+
+        if (gol_update_count % GOL_EVOLVE_PERIOD_MOD == 0) {
+            for (size_t i = 0; i < OLED_DISPLAY_WIDTH; i++) {
+                for (size_t j = 0; j < OLED_DISPLAY_HEIGHT; j++) {
+                    oled_write_pixel(i, j, gol_state[i / GOL_SQUARE_SIZE][j / GOL_SQUARE_SIZE]);
+                }
+            }
+
+            gol_play_game();
+        }
+        gol_update_count++;
+
+    } else if (gol_update_count == GOL_UPDATE_MAX_COUNT) {
+        oled_write_raw(screen_outline, OLED_MATRIX_SIZE);
+        gol_update_count++;
     } else {
-        if (write_outline) {
-            oled_write_raw(screen_outline, OLED_MATRIX_SIZE);
-            write_outline = false;
+        /* Set WPM rate */
+        uint8_t wpm  = get_current_wpm();
+        uint8_t bars = 0;
+        if (wpm > SPEED_MAX_BARS_WPM) {
+            bars = SPEED_NUM_BARS;
         } else {
-            /* Set WPM rate */
-            uint8_t wpm  = get_current_wpm();
-            uint8_t bars = 0;
-            if (wpm > SPEED_MAX_BARS_WPM) {
-                bars = SPEED_NUM_BARS;
+            bars = wpm / SPEED_WPM_TO_BARS;
+        }
+
+        for (size_t i = 0; i < SPEED_NUM_BARS; i++) {
+            if (bars) {
+                oled_write_raw_byte(0xFF, 264 + i);
+                bars--;
             } else {
-                bars = wpm / SPEED_WPM_TO_BARS;
+                oled_write_raw_byte(0x00, 264 + i);
             }
+        }
 
-            for (size_t i = 0; i < SPEED_NUM_BARS; i++) {
-                if (bars) {
-                    oled_write_raw_byte(0xFF, 264 + i);
-                    bars--;
-                } else {
-                    oled_write_raw_byte(0x00, 264 + i);
+        /* Set Caps Lock */
+        led_t       leds = host_keyboard_led_state();
+        const char* caps_glyph;
+        if (leds.caps_lock) {
+            caps_glyph = caps_lock_on;
+        } else {
+            caps_glyph = caps_lock_off;
+        }
+
+        for (size_t i = 0; i < CAPS_LOCK_ROWS_BYTES; i++) {
+            for (size_t j = 0; j < CAPS_LOCK_COLS_BITS; j++) {
+                oled_write_raw_byte(caps_glyph[i * CAPS_LOCK_COLS_BITS + j], (OLED_DISPLAY_WIDTH + 50) + OLED_DISPLAY_WIDTH * i + j);
+            }
+        }
+
+        /* Set Layer (Currently only supports 0-3 layers) */
+        uint8_t lay = 0;
+        lay         = lay | layer_state_is(0) << 0;
+        lay         = lay | layer_state_is(1) << 1;
+        lay         = lay | layer_state_is(2) << 2;
+        lay         = lay | layer_state_is(3) << 3;
+
+        const char* layer_glyph;
+
+        switch (lay) {
+            case 0b0001:
+                layer_glyph = layer_num_0;
+                break;
+            case 0b0010:
+                layer_glyph = layer_num_1;
+                break;
+            case 0b0100:
+                layer_glyph = layer_num_2;
+                break;
+            case 0b1000:
+                layer_glyph = layer_num_3;
+                break;
+
+            default:
+                layer_glyph = layer_num_0;
+                break;
+        }
+
+        for (size_t i = 0; i < LAYER_ROWS_BYTES; i++) {
+            for (size_t j = 0; j < LAYER_COLS_BITS; j++) {
+                oled_write_raw_byte(layer_glyph[i * LAYER_COLS_BITS + j], (OLED_DISPLAY_WIDTH + 98) + OLED_DISPLAY_WIDTH * i + j);
+            }
+        }
+    }
+}
+
+
+/* 
+ * John Conway's Game of Life
+ * https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+ * C implementation inspired by:
+ * https://rosettacode.org/wiki/Conway%27s_Game_of_Life#C
+ */
+void gol_play_game(void) {
+    for (size_t i = 0; i < GOL_WIDTH; i++) {
+        for (size_t j = 0; j < GOL_HEIGHT; j++) {
+            size_t n = 0;
+
+            /* Since the state is stored in a uint8_t, we use the first bit for the current state */
+            /* and the second bit as the next state */
+            for (size_t x1 = i - 1; x1 <= i + 1; x1++) {
+                for (size_t y1 = j - 1; y1 <= j + 1; y1++) {
+                    if (gol_state[(x1 + GOL_WIDTH) % GOL_WIDTH][(y1 + GOL_HEIGHT) % GOL_HEIGHT] & 0x01) {
+                        n++;
+                    }
                 }
             }
 
-            /* Set Caps Lock */
-            led_t       leds = host_keyboard_led_state();
-            const char* caps_glyph;
-            if (leds.caps_lock) {
-                caps_glyph = caps_lock_on;
-            } else {
-                caps_glyph = caps_lock_off;
+            if (gol_state[i][j] & 0x01) {
+                n--;
             }
+            gol_state[i][j] |= (n == 3 || (n == 2 && (gol_state[i][j] & 0x01))) << 1;
+        }
+    }
 
-            for (size_t i = 0; i < CAPS_LOCK_ROWS_BYTES; i++) {
-                for (size_t j = 0; j < CAPS_LOCK_COLS_BITS; j++) {
-                    oled_write_raw_byte(caps_glyph[i * CAPS_LOCK_COLS_BITS + j], (OLED_DISPLAY_WIDTH + 50) + OLED_DISPLAY_WIDTH * i + j);
-                }
-            }
+    /* second bit is shifted into the 1st bit position and the second bit is cleared */
+    for (size_t i = 0; i < GOL_WIDTH; i++) {
+        for (size_t j = 0; j < GOL_HEIGHT; j++) {
+            gol_state[i][j] = (gol_state[i][j] >> 1) & 0x01;
+        }
+    }
+}
 
-            /* Set Layer (Currently only supports 0-3 layers) */
-            uint8_t lay = 0;
-            lay         = lay | layer_state_is(0) << 0;
-            lay         = lay | layer_state_is(1) << 1;
-            lay         = lay | layer_state_is(2) << 2;
-            lay         = lay | layer_state_is(3) << 3;
-
-            const char* layer_glyph;
-
-            switch (lay) {
-                case 0b0001:
-                    layer_glyph = layer_num_0;
-                    break;
-                case 0b0010:
-                    layer_glyph = layer_num_1;
-                    break;
-                case 0b0100:
-                    layer_glyph = layer_num_2;
-                    break;
-                case 0b1000:
-                    layer_glyph = layer_num_3;
-                    break;
-
-                default:
-                    layer_glyph = layer_num_0;
-                    break;
-            }
-
-            for (size_t i = 0; i < LAYER_ROWS_BYTES; i++) {
-                for (size_t j = 0; j < LAYER_COLS_BITS; j++) {
-                    oled_write_raw_byte(layer_glyph[i * LAYER_COLS_BITS + j], (OLED_DISPLAY_WIDTH + 98) + OLED_DISPLAY_WIDTH * i + j);
-                }
-            }
+void gol_randomize(void) {
+    for (size_t i = 0; i < (GOL_WIDTH); i++) {
+        uint32_t rn = rand();
+        for (size_t j = 0; j < GOL_HEIGHT; j++) {
+            gol_state[i][j] = (rn >> j) & 0x01;
         }
     }
 }
