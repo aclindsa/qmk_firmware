923f6fbd9dabc02d104e37140a3385e38cdcfbde
Aaron Lindsay <aaron@os.amperecomputing.com>
2021-02-01 13:31:01 -0500

    Enable WFI on idle

--- keyboards/kobuss/grabert/rules.mk
+++ keyboards/kobuss/grabert/rules.mk
@@ -9,7 +9,7 @@ EXTRAKEY_ENABLE = yes
 OLED_DRIVER_ENABLE = yes
 WPM_ENABLE = yes
 
-# Laptop users may want to enable low-power on idle thread
-# OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+# Enable low-power on idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
 DFU_ARGS = -d 0483:df11 -a 0 -s 0x08000000:leave
